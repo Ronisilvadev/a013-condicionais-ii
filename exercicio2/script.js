040a0e07efecc7e6f321f932c378de2941ac714b
roni <ronildo.sad2@hotmail.com>
2022-08-09 14:02:24 -0300

    enviando exercicio praticado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,47 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+// let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
+// if(nacionalidade === "brasileira"){
+//     console.log("a pessoa é do Brasil!");
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+
+
+// }
+
+
+let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase()
+
+
+switch(nacionalidade){
+    case "brasil":
     console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
+    break
+
+    case "argentina":
     console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
+    break
+
+    case "aruguai":
     console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
+    break
+
+    case "chile":
     console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
+    break
+
+    case "colômbia":
     console.log("a pessoa é da Colômbia!");
-} else{
+    break
+
+    default:
     console.log("nacionalidade não encontrada")
-}
+
+    }
